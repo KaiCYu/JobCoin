ebedb1f7b45fd78f60a343d73acc45eaefb2daa6
KaiCYu <kaichyu@gmail.com>
2017-08-15 16:54:31 -0700

    added event handler for form

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -6,22 +6,36 @@ import List from './components/List.jsx';
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { 
-      items: []
+    this.state = {
+      address: '',
     }
+    this.onSubmit = this.onSubmit.bind(this);
+    this.onInputChange = this.onInputChange.bind(this);
   }
 
-  componentDidMount() {
-    $.ajax({
-      url: '/items', 
+  // componentDidMount() {
+    
+  // }
+
+  onSubmit() {
+    //send ajax call with address to API
+    console.log('state:', this.state.address);
+    $.get({
+      url: `http://jobcoin.projecticeland.net/dinosaur/api/addresses/${this.state.address}`,
       success: (data) => {
-        this.setState({
-          items: data
-        })
+        console.log('got data back!', data);
       },
       error: (err) => {
         console.log('err', err);
       }
+    })
+    //take you to next screen
+  }
+
+  onInputChange(event) {
+    console.log('name', event.target.value);
+    this.setState({
+      address: event.target.value,
     });
   }
 
@@ -29,7 +43,16 @@ class App extends React.Component {
     return (<div>
       <h1>Item List</h1>
       <h1> Kai Yu</h1>
-      <List items={this.state.items}/>
+
+      <div>
+        <p>Welcome! Sign in with your Job Coin Address</p>
+
+        <form>
+          <input type="text" onChange={this.onInputChange} value={this.state.address}/>
+          <button onClick={this.onSubmit} >Sign In</button>
+        </form>
+      </div>
+
     </div>)
   }
 }
