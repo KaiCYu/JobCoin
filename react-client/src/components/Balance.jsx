46993c63e007b37795b93020e92be35d80ca7a9f
KaiCYu <kaichyu@gmail.com>
2017-08-17 16:31:20 -0700

    added react router to move between pages

--- react-client/src/components/Balance.jsx
+++ react-client/src/components/Balance.jsx
@@ -3,6 +3,9 @@ import React from 'react';
 const Balance = (props) => (
   <div>
     <h1>  HI </h1>
+    {
+      console.log(props)
+    }
   </div>
 )
 
--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -1,49 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
+// import {browserHistory} from 'react-router';
 import { BrowserRouter as Router, Route, Link, Redirect } from 'react-router-dom';
+import Login from './components/Login.jsx';
 import Balance from './components/Balance.jsx';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { 
-      address: '',
-      userData: {},
-    }
-    this.handleInputChange = this.handleInputChange.bind(this);
-    this.handleGetRequest = this.handleGetRequest.bind(this);
-  }
-
-  handleInputChange(e){
-    this.setState({address: e.target.address});
-  }
-
-  handleGetRequest(){
-    $.get({
-      url: `http://jobcoin.projecticeland.net/dinosaur/api/addresses/${this.state.value}`,
-      success: function (data) {
-        console.log('success',data);
-        this.setState({ userData: data});
-      }.bind(this),
-      error: function (err) {
-        console.log('err', err)
-      }
-    })
-  };
-
   render () {
     return (
       <div>
         <Router>
           <div>
-            <h3> Welcome! Sign in with your JobCoin Address </h3>
-            <p> JobCoin Address </p>
-
-            <input type="text" name="name" onChange={this.handleInputChange} value={this.state.address}/>
-            <button onClick={this.handleGetRequest}>Submit</button>
-            <Route path="/Balance" component={Balance}/>
-            {/*<Link to="/Balance" onClick={this.handleGetRequest} value={this.state.address}>Submit</Link>*/}
+            <Route exact path="/" component={Login}/>
+            <Route path="/balance" component={Balance}/>
           </div>
         </Router>
       </div>
