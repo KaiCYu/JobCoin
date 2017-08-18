b70eba913ae1b64f65c4156c4ed742623e47b345
KaiCYu <kaichyu@gmail.com>
2017-08-17 20:51:49 -0700

    added chart

--- react-client/src/components/Chart.js
+++ react-client/src/components/Chart.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import chartJS from 'react-chartjs';
+import {Line as LineChart} from 'react-chartjs';
 
-var LineChart = require("react-chartjs").Line;
 // var LineChart = require("react-chartjs").Line;
 
-
 const ChartComponent = (props)=>{
-  return <LineChart data={props.chartData} options={props.option} width="600" height="250"/> 
+  return (
+    <div>
+      {console.log('chart props', props.chartData)}
+      <LineChart data={set1} options={null} width="600" height="250"/> 
+    </div>
+  )
 };
 
 export default ChartComponent;
+
+const set1 = {
+  // labels: ["January", "February", "March", "April", "May", "June", "July"],
+  datasets: [
+    {
+      label: "My First dataset",
+      fill: true,
+      pointHoverRadius: 5,
+      pointRadius: 1,
+      pointHitRadius: 10,
+      data: [65, 59, 80, 81, 56, 55, 40],
+      spanGaps: false,
+    }
+  ]
+};
--- react-client/src/components/LoginPage.js
+++ react-client/src/components/LoginPage.js
@@ -16,10 +16,12 @@ class LoginPage extends React.Component {
       userData: [1,2,3,4,5,6],
       userInfor: [10, 20, 30 , 40]
     }
+
+    this.initialState = this.state;
     this.handleInputChange = this.handleInputChange.bind(this);
     this.handleGetRequest = this.handleGetRequest.bind(this);
     this.handleUserChange = this.handleUserChange.bind(this);
-    this.handleJobCoinChange = this.handleJobCoinChange.bind(this);
+    this.handleSendCoin = this.handleSendCoin.bind(this);
     this.handleCoinChange = this.handleCoinChange.bind(this);
   }
 
@@ -32,8 +34,12 @@ class LoginPage extends React.Component {
   handleCoinChange(e){
     this.setState({sendingCoinNumber: e.target.value})
   }
-  handleJobCoinChange(){
-    // console.log(this.state)
+
+  handleSignout() {
+    this.setState(this.initialState);
+  }
+
+  handleSendCoin(){
     var data = {
       fromAddress: this.state.value,
       toAddress: this.state.sendingUserName,
@@ -51,9 +57,11 @@ class LoginPage extends React.Component {
       }
     })
   }
+
   getDataForChart(){
 
   }
+
   handleGetRequest(){
     $.get({
       url: `http://jobcoin.projecticeland.net/dinosaur/api/addresses/${this.state.value}`,
@@ -70,35 +78,38 @@ class LoginPage extends React.Component {
   };
 
   render () {
-
+      //not logged in
       { if(this.state.userInformation === ''){
         return (
           <div>
             <h3> Welcome! Sign in with your JobCoin Address </h3>
             <p> JobCoin Address </p>
             <input type="text" onChange={this.handleInputChange} value={this.state.value}/>
-            <button onClick={this.handleGetRequest}>Button </button>
+            <button onClick={this.handleGetRequest}>Sign In </button>
           </div>
         )
+      //logged in
       } else {
         return (
           <div>
-
             <div>
+              <button onClick={this.handleSignout}> Sign Out </button>
               <h1> Welcome User: {this.state.value} </h1>
               <h2> JobCoin Balance </h2>
               <h2> {this.state.userInformation.balance} </h2>
             </div>
             <div>
               <h2> Send JobCoin </h2>
-              <input type="text" onChange={this.handleUserChange} value={this.state.sendingUserName}/>
-              <input type="text" onChange={this.handleCoinChange} value={this.state.sendingCoinNumber}/>
-              <button onClick={this.handleJobCoinChange}> Send JobCoin </button>
-              <ChartComponent chartData={this.state.userInfor} options={this.state.userData}/>
+              To:<input type="text" onChange={this.handleUserChange} value={this.state.sendingUserName}/>
+              Amount:<input type="text" onChange={this.handleCoinChange} value={this.state.sendingCoinNumber}/>
+              <button onClick={this.handleSendCoin}> Send JobCoin </button>
             </div>
+              
+            <ChartComponent chartData={this.state.userInfor} options={this.state.userData}/>
           </div>
         )
-      }}
+      }
+    }
   }
 }
 
